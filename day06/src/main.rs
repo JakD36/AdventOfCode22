c4e5fde0c14604f61211c29bfa7887697c7d0f6d
Jack Davidson <jack.davidson@tag-games.com>
2022-12-06 21:49:08 +0000

    Testing a few variations on day 06 to measure performance

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,13 +1,48 @@
+#![allow(dead_code)]
+
+use std::time::Instant;
+use rayon::prelude::*;
 
 
 fn main() {
     let filepath = "input.txt";
     let signal = std::fs::read_to_string(filepath)
         .expect(format!("Failed to read {}", filepath).as_str());
+
+    println!("Using Skips + For Loop");
+    let start = Instant::now();
     let part1 = find_start_of_marker(signal.as_str(), MarkerType::PACKET);
+    println!("Part 1 = {}, calculated in {} us", part1, (Instant::now() - start).as_micros());
+    let start = Instant::now();
     let part2 = find_start_of_marker(signal.as_str(), MarkerType::MESSAGE);
-    println!("Part 1 = {}", part1);
-    println!("Part 2 = {}", part2);
+    println!("Part 2 = {}, calculated in {} us", part2, (Instant::now() - start).as_micros());
+    println!();
+    
+    println!("Using iterators");
+    let start = Instant::now();
+    let part1 = iter_find_start_of_marker(signal.as_str(), MarkerType::PACKET);
+    println!("Part 1 = {}, calculated in {} us", part1, (Instant::now() - start).as_micros());
+    let start = Instant::now();
+    let part2 = iter_find_start_of_marker(signal.as_str(), MarkerType::MESSAGE);
+    println!("Part 2 = {}, calculated in {} us", part2, (Instant::now() - start).as_micros());
+    println!();
+    
+    println!("Using parallel iterators in rayon");
+    let start = Instant::now();
+    let part1 = par_find_start_of_marker(signal.as_str(), MarkerType::PACKET);
+    println!("Part 1 = {}, calculated in {} us", part1, (Instant::now() - start).as_micros());
+    let start = Instant::now();
+    let part2 = par_find_start_of_marker(signal.as_str(), MarkerType::MESSAGE);
+    println!("Part 2 = {}, calculated in {} us", part2, (Instant::now() - start).as_micros());
+    println!();
+
+    println!("Using Skips + For Loop");
+    let start = Instant::now();
+    let part1 = find_start_of_marker_with_skip(signal.as_str(), MarkerType::PACKET);
+    println!("Part 1 = {}, calculated in {} us", part1, (Instant::now() - start).as_micros());
+    let start = Instant::now();
+    let part2 = find_start_of_marker_with_skip(signal.as_str(), MarkerType::MESSAGE);
+    println!("Part 2 = {}, calculated in {} us", part2, (Instant::now() - start).as_micros());
 }
 
 #[derive(Clone, Copy)]
@@ -23,7 +58,7 @@ fn find_start_of_marker(signal: &str, _type: MarkerType) -> u32
     
     assert!(signal.len() >= unique_set_len as usize, "Input signal is expected to be greater than {} characters long.", unique_set_len);
     let chars = signal.chars().collect::<Vec<char>>();
-
+    
     for i in unique_set_len as usize..signal.len()
     {
         let mut bitmask = 0;
@@ -46,6 +81,110 @@ fn find_start_of_marker(signal: &str, _type: MarkerType) -> u32
     return 0;
 }
 
+
+fn iter_find_start_of_marker(signal: &str, _type: MarkerType) -> u32
+{
+    let unique_set_len = _type as u32;
+    
+    let chars = signal.chars().collect::<Vec<char>>();
+    let mut iter = signal.char_indices();
+    iter.nth(unique_set_len as usize).expect(format!("Input signal is expected to be greater than {} characters long.", unique_set_len).as_str());
+    let found_thing = iter.find(|(i, _)|  {
+        let mut bitmask = 0;
+        for j in 0..unique_set_len as usize
+        {
+            bitmask |= 1 << (chars[*i as usize - j as usize] as u32 - 'a' as u32);
+        }
+
+        // check bitmask
+        let mut count = 0;
+        for j in 0..26
+        {
+            count += ((bitmask & 1 << j) > 0) as u32
+        }
+        if count == unique_set_len
+        {
+            return true
+        }
+        return false;
+    });
+
+    let (idx, _) = found_thing.expect("Failed to find an answer!");
+    return idx as u32 + 1;
+}
+
+
+fn par_find_start_of_marker(signal: &str, _type: MarkerType) -> u32
+{
+    let unique_set_len = _type as u32;
+    assert!(signal.len() >= unique_set_len as usize, "Input signal is expected to be greater than {} characters long.", unique_set_len);
+    
+    let chars = signal.chars().collect::<Vec<char>>();
+    let found_thing = signal.par_char_indices().find_first(|(i, _)|  {
+       if *i < unique_set_len as usize
+       {
+           return false;
+       }
+        
+        let mut bitmask = 0;
+        for j in 0..unique_set_len as usize
+        {
+            bitmask |= 1 << (chars[*i as usize - j as usize] as u32 - 'a' as u32);
+        }
+
+        // check bitmask
+        let mut count = 0;
+        for j in 0..26
+        {
+            count += ((bitmask & 1 << j) > 0) as u32
+        }
+        if count == unique_set_len
+        {
+            return true
+        }
+        return false;
+    });
+    
+    let (idx, _) = found_thing.expect("Failed to find an answer!");
+    return idx as u32 + 1;
+}
+
+fn find_start_of_marker_with_skip(signal: &str, _type: MarkerType) -> u32
+{
+    let unique_set_len = _type as u32;
+    assert!(signal.len() >= unique_set_len as usize, "Input signal is expected to be greater than {} characters long.", unique_set_len);
+    assert!(unique_set_len < 256, "Marker size can't be longer than 256 as we store our indices using a u8");
+    
+    let mut char_found_at_index = [0u8; 26];
+    let chars = signal.chars().collect::<Vec<char>>();
+    
+    let mut look_back = unique_set_len as usize;
+    let mut idx = unique_set_len as usize - 1;
+    'outer :while idx < chars.len()
+    {
+        for j in (0..look_back).rev()
+        {
+            let char_index = chars[idx - j] as usize - 'a' as usize;
+            if char_found_at_index[char_index] > 0
+            {
+                let skip_by= char_found_at_index[char_index];
+                char_found_at_index[char_index] = (unique_set_len - j as u32) as u8;// +1 to adjust skip by ahead of time
+                idx += skip_by as usize;
+                look_back = skip_by as usize + j;
+                
+                char_found_at_index.iter_mut().for_each(|x| *x = std::cmp::max(*x as i16 - (skip_by as i16), 0) as u8);
+                continue 'outer;
+            }
+            else
+            {
+                char_found_at_index[char_index] = (unique_set_len - j as u32) as u8; // +1 to adjust skip by ahead of time
+            }
+        }
+        return idx as u32 + 1;
+    }
+    
+    return 0;
+}
 
 #[test]
 fn part1_test_signal0()
